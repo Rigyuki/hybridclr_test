ece20390d528ca0267f9edb131f20a02940aa1ac
walon <taojingjian@gmail.com>
2022-07-07 14:22:41 +0800

    - 更新背景图 - 添加反射获取字符串常量字段的测试用例 - 更新安装脚本

---
+++ Assets/Tests/CSharp/TC_Reflection.cs
@@ -0,0 +1,26 @@
+﻿using SharpUnit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.CSharp
+{
+
+    class ConstString
+    {
+        public const string Str = "abc";
+    }
+
+    internal class TC_Reflection : GeneralTestCaseBase
+    {
+        [UnitTest]
+        public void const_string()
+        {
+            var strField = typeof(ConstString).GetField("Str");
+            string s = (string)strField.GetValue(null);
+            Assert.Equal("abc", s);
+        }
+    }
+}
