7042bfa28e0612354399b6ee96789b3ab4356e5c
walon <walon@focus-creative-games.com>
2022-07-23 20:41:07 +0800

    [new] 添加 interface default method相关测试用例

---
+++ Assets/Tests/CSharp/TC_InterfaceDefaultMethod.cs
@@ -0,0 +1,34 @@
+﻿using SharpUnit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.CSharp
+{
+#if UNITY_2021_1_OR_NEWER
+    public interface IShow
+    {
+        int Show(int a)
+        {
+            return a + 1;
+        }
+    }
+
+    public class MyShow : IShow
+    {
+
+    }
+
+    internal class TC_InterfaceDefaultMethod : GeneralTestCaseBase
+    {
+        [UnitTest]
+        public void test()
+        {
+            IShow a = new MyShow();
+            Assert.Equal(11, a.Show(10));
+        }
+    }
+#endif
+}
