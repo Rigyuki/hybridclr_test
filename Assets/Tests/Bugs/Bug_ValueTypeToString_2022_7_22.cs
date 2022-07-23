447e2eb218c40768fff010779f9cf2ee29d5a288
walon <walon@focus-creative-games.com>
2022-07-23 11:19:20 +0800

    [fix] 新增AOT ValueType 成员函数补充元数据的示例

---
+++ Assets/Tests/Bugs/Bug_ValueTypeToString_2022_7_22.cs
@@ -0,0 +1,21 @@
+﻿using SharpUnit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Bugs
+{
+
+    internal class Bug_ValueTypeToString_2022_7_22 : GeneralTestCaseBase
+    {
+        [UnitTest]
+        public void tostring()
+        {
+            var p = new KeyValuePair<ulong, int>(1, 2);
+            var s = p.ToString();
+            Assert.Equal("[1, 2]", s);
+        }
+    }
+}
