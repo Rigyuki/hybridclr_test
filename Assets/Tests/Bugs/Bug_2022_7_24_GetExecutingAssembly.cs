e9629f5e230cc48f0dc7c9c83932503b11b63ed1
walon <walon@focus-creative-games.com>
2022-07-25 16:08:11 +0800

    [new] 新增 Assembly.GetExecutingAssembly的测试用例（但注释掉了，因为il2cpp下实现是错的）

---
+++ Assets/Tests/Bugs/Bug_2022_7_24_GetExecutingAssembly.cs
@@ -0,0 +1,24 @@
+﻿using AOTDefs;
+using SharpUnit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Tests.Bugs
+{
+
+    internal class Bug_2022_7_24_GetExecutingAssembly : GeneralTestCaseBase
+    {
+        /*
+        [UnitTest]
+        public void test()
+        {
+            var ass = System.Reflection.Assembly.GetExecutingAssembly();
+            Assert.Equal("Tests", ass.GetName().Name);
+        }
+        */
+    }
+}
